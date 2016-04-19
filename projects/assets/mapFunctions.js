9826604803b0d7bdba98d5b5cbb1123e0bfb2d43
plablo09 <pablo.lopez@centrogeo.edu.mx>
2016-04-18 20:34:00 -0500

    Gráficas de pie.

---
+++ projects/assets/mapFunctions.js
@@ -0,0 +1,122 @@
+function playback(index,maxIndex) {
+    $(":radio[value="+locations[index].radioId+"]").prop('checked',true);
+	  ClearChart();
+    // Animate the map position based on camera properties
+    map.flyTo(locations[index].camera);
+    map.once('moveend', function() {
+		    getFeatures();
+        // Duration the slide is on screen after interaction
+        myTimerFly = window.setTimeout(function() {
+            // Increment index
+            index = (index + 1 === locations.length) ? 0 : index + 1;
+            if(index != maxIndex){
+                playback(index,maxIndex);
+            }else {
+				        if (myTimerFly != null) stopTravel();
+				        //if (myTimerFly != null) stopTravel();
+                map.flyTo(locations[locations.length - 1].camera);
+                $(":radio[value=general]").prop('checked',true)
+                $('input[name=place]').attr("disabled",false);
+            }
+        }, 10000); // After callback, show the location for 10 seconds.
+    });
+}
+
+function flyToLocation(){
+	if (myTimerFly != null) stopTravel();
+    radioValue = $(this).val();
+    locations.forEach(function(item,index){
+        if(item["radioId"] == radioValue){
+            map.flyTo(locations[index].camera);
+        }
+    });
+}
+
+function stopTravel(){
+	clearTimeout(myTimerFly);
+	myTimerFly = null;
+    map.on('movestart', function() {
+		ClearChart();
+    });
+    map.on('moveend', function() {
+		ClearChart();
+		getFeatures();
+    });
+}
+
+function Count(type){
+
+	switch (type) {
+		case 1:
+		    NegCounter = NegCounter + 1;
+		    break;
+		case 2:
+		    NeuCounter = NeuCounter + 1;
+		    break;
+		case 3:
+		    PosCounter = PosCounter + 1;
+		    break;
+	}
+
+}
+
+function RefreshGraph(){
+
+			chartData.push({
+				type: 	"Negativo",
+				count: 	NegCounter,
+				color:  "#C81316"
+			});
+			chartData.push({
+				type: 	"Neutro",
+				count: 	NeuCounter,
+				color:  "#238AA3"
+			});
+
+			chartData.push({
+				type: 	"Positivo",
+				count: 	PosCounter,
+				color:  "#98A243"
+			});
+
+			chart.validateData();
+
+}
+
+function ClearChart(){
+
+	PosCounter = 0;
+	NegCounter = 0;
+	NeuCounter = 0;
+
+	chartData = [];
+	chart.dataProvider = chartData;
+	chart.validateData();
+
+}
+
+function getFeatures(){
+
+	// Negativos...
+	map.featuresIn({ layer: 'clase 1' }, function(err, features) {
+		for (i in features){
+			Count(1);
+		}
+	});
+
+	// Neutros...
+	map.featuresIn({ layer: 'clase 2' }, function(err, features) {
+		for (i in features){
+			Count(2);
+		}
+	});
+
+	// Positivos...
+	map.featuresIn({ layer: 'clase 3' }, function(err, features) {
+		for (i in features){
+			Count(3);
+		}
+		RefreshGraph();
+	});
+
+}
